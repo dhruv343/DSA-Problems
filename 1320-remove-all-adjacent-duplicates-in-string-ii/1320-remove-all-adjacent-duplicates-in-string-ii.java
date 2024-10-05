596c01d450b162c448876868cb22ccffbde96ff5
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-06 01:59:18 +0530

    Time: 43 ms (36.47%), Space: 45.6 MB (52.96%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    class Pair{
+        char c;
+        int count;
+
+        Pair(char c,int count){
+            this.c=c;
+            this.count=count;
+        }
+    }
+    public String removeDuplicates(String s, int k) {
+        
+        Stack<Pair> stack=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+
+            int duplicates=0;
+            while(!stack.isEmpty() && stack.peek().c==s.charAt(i)){
+            duplicates=stack.peek().count;
+            stack.pop();
+            }
+
+            if(duplicates+1<k){
+            stack.push(new Pair(s.charAt(i),duplicates+1));
+            }
+            
+        }
+
+        while(!stack.isEmpty()){
+            Pair p=stack.pop();
+            char c=p.c;
+            int count=p.count;
+
+            while(count!=0){
+                sb.append(c);
+                count--;
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
