8076106c1ea5161427339aa60a9df19a9f358371
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-05 19:43:55 +0530

    Time: 57 ms (67.04%), Space: 53.8 MB (5.63%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> s=new Stack<>();
+        int[] psl=new int[arr.length];
+        int[] nsl=new int[arr.length];
+
+        Arrays.fill(psl,-1);
+        Arrays.fill(nsl,arr.length);
+        
+        //finding psl for every element
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && arr[i]<arr[s.peek()]){
+            s.pop();
+            }
+            psl[i]=s.isEmpty()?-1:s.peek();
+            s.push(i);
+        }
+        s.clear();
+        //finding nsl for every element 
+        for(int i=0;i<arr.length;i++){
+            while(!s.empty() && arr[i]<arr[s.peek()]){
+            int ele=s.pop();
+            nsl[ele]=i;
+            }
+            s.push(i);
+        }
+
+
+        long answer=0;
+        int mod=(int)1e9+7;
+        for(int i=0;i<arr.length;i++){
+            
+            long left=i-psl[i];
+            long right=nsl[i]-i;
+            
+            long contribution=((left*right)%mod*arr[i])%mod;
+            
+            answer = (answer + contribution) % mod;
+
+        }
+
+        return (int)answer;
+    }
+}
