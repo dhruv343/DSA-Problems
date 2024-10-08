2c900b76edae483ffed378cc2fdccb51f25c0596
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-09 04:44:04 +0530

    Time: 0 ms (100%), Space: 51.4 MB (25.34%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.java
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -24,10 +24,10 @@ class Solution {
         if(root.val>=low && root.val<=high){
             return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
         }
-        else if(root.val>low && root.val>high){
+        else if(root.val>high){
             return rangeSumBST(root.left,low,high);
         }
-        else if(root.val<low && root.val<high){
+        else if(root.val<low){
             return rangeSumBST(root.right,low,high);
         }
         
