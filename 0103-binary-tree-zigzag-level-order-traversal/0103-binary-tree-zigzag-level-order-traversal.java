dd104d0ffe20b85c1c1e38ac9703c44a0ae24ca2
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-10 04:22:08 +0530

    Time: 1 ms (73.53%), Space: 42 MB (77.17%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,58 +1,58 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
-
-
-        Queue<TreeNode> queue=new LinkedList<>();
-        List<List<Integer>> answer=new ArrayList<>();
-        boolean reverse=true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        boolean reverse = false;
         queue.offer(root);
-        if(root==null){
+
+        if (root == null) {
             return answer;
         }
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
 
-            int size=queue.size(); ///no of nodes on current level
-            List<Integer> ele=new ArrayList<>();
-            reverse=!reverse;
+            int size = queue.size(); /// no of nodes on current level
+            List<Integer> ele = new ArrayList<>(size);
 
-            for(int i=0;i<size;i++){
+            for (int i = 0; i < size; i++) {
 
-            TreeNode node=queue.remove();
-            ele.add(node.val);
+                TreeNode node = queue.remove();
 
-            if(node.left!=null){
-            queue.offer(node.left);
+                if (reverse) {
+                    ele.add(0, node.val); // Add to the front for reverse order
+                } else {
+                    ele.add(node.val); // Add to the end for normal order
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
             }
-            if(node.right!=null){
-                queue.offer(node.right);
-            }
 
-            }
-
-            if(reverse){
-            Collections.reverse(ele);
-            }
-
+            reverse = !reverse;
             answer.add(ele);
         }
-        
+
         return answer;
     }
 }
