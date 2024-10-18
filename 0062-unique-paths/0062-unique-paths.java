2b680e62363dca3dc0e53b5899c9a21712911c78
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-19 01:00:10 +0530

    Time: 0 ms (100%), Space: 40.8 MB (12.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,43 @@
 class Solution {
     
-    public int function(int i,int j,int[][] dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
+    // public int function(int i,int j,int[][] dp){
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
+    //     if(i<0 || j<0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        return dp[i][j]=function(i,j-1,dp)+function(i-1,j,dp);
-    }
+    //     return dp[i][j]=function(i,j-1,dp)+function(i-1,j,dp);
+    // }
 
     public int uniquePaths(int m, int n) {
         
         int[][] dp=new int[m][n];
 
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left=0;
+
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+
+                dp[i][j]=up+left;
+            }
         }
-        return function(m-1,n-1,dp);
+
+        return dp[m-1][n-1];
         
     }
 }
