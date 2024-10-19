784bd4e9d306049b98948b0e3d26d02b18f2821d
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-19 22:37:14 +0530

    Time: 58 ms (39.25%), Space: 46.5 MB (61.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,31 +1,34 @@
 class Solution {
 
-    public boolean f(int index,int target,int[] nums,int[][] dp){
-    
-    if(target==0) return true;
-    if(index==0) return nums[0]==target;
-    
-    if(dp[index][target]!=-1){
-    return dp[index][target]==0?false:true;
+    public boolean f(int index, int target, int[] nums, int[][] dp) {
+
+        if (target == 0)
+            return true;
+        if (index == 0)
+            return nums[0] == target;
+
+        if (dp[index][target] != -1) {
+            return dp[index][target] == 0 ? false : true;
+        }
+
+        // not take
+        boolean notTake = f(index - 1, target, nums, dp);
+
+        // take
+        boolean take = false;
+        if (nums[index] <= target) {
+            take = f(index - 1, target - nums[index], nums, dp);
+        }
+
+        if (take || notTake) {
+            dp[index][target] = 1;
+            return true;
+        }
+        dp[index][target] = 0;
+        return false;
+
     }
 
-    //not take
-    boolean notTake=f(index-1,target,nums,dp);
-
-    //take
-    boolean take=false;
-    if(nums[index]<=target){
-        take=f(index-1,target-nums[index],nums,dp);
-    }
-
-    if(take || notTake){
-        dp[index][target]=1;
-        return true;
-    }
-    dp[index][target]=0;
-    return false;
-
-    }
     public boolean canPartition(int[] nums) {
         
         int sum=0;
@@ -37,11 +40,34 @@ class Solution {
         int n=nums.length;
         if(sum%2!=0) return false;
         
-        int[][] dp=new int[n][target+1];
-        for(int[] arr:dp){
-        Arrays.fill(arr,-1);
+        boolean[][] dp=new boolean[n][target+1];
+        for(boolean[] arr:dp){
+        Arrays.fill(arr,false);
+        }
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target){
+            dp[0][nums[0]]=true;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+             //not take
+            boolean notTake=dp[i-1][j];
+
+            //take
+            boolean take=false;
+            if(nums[i]<=j){
+                take=dp[i-1][j-nums[i]];
+            }
+
+            dp[i][j]=take || notTake;
+            }
         }
 
-        return f(n-1,target,nums,dp);
+        return dp[n-1][target];
+
     }
 }
