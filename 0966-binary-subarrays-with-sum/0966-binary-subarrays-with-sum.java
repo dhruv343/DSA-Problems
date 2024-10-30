312da0299b6432fa5349ae546d2cea84795b4c82
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-10-31 03:34:39 +0530

    Time: 1 ms (100%), Space: 47.2 MB (85.69%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,21 +1,34 @@
 class Solution {
+
+    public int func(int[] nums,int goal){
+
+    int l=0;
+    int r=0;
+    int count=0;
+    int sum=0;
+    if(goal<0){
+        return 0;
+    }
+
+    while(r<nums.length){
+       sum+=nums[r];
+
+       //sum > goal
+       while(sum>goal){
+        sum-=nums[l];
+        l++;
+       }
+
+       //sum is less than and equal to goal
+       count+=(r-l+1);
+       r+=1;
+    }
+
+    return count;
+
+    }
+
     public int numSubarraysWithSum(int[] nums, int goal) {
-        
-
-        int number=0;
-
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-
-                if(sum==goal){
-                    number++;
-                }
-            }
-        }
-
-        return number;
+        return func(nums,goal)-func(nums,goal-1);
     }
 }
