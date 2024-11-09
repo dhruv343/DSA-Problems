569436ab3656d9e1e7e46000d12631d576f84b09
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-11-09 19:47:56 +0530

    Time: 450 ms (6.12%), Space: 57.5 MB (8.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,20 +4,29 @@ class Solution {
         int sum=0;
         int maxSum=Integer.MIN_VALUE;
         int start=-1;
-        int end=-1;
+        int ansStart=-1;
+        int ansEnd=-1;
 
         for(int i=0;i<nums.length;i++){
-
+        if(sum==0){
+            start=i;
+        }
         sum+=nums[i];
 
         if(sum>maxSum){
             maxSum=sum;
+            ansStart=start;
+            ansEnd=i;
         }
         
         if(sum<0){
             sum=0;
         }
 
+        }
+
+        for(int i=ansStart;i<=ansEnd;i++){
+            System.out.print(nums[i]+" ");
         }
 
         return maxSum;
