2545430d1249a93b0b5bb787702d7a88e727157f
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-11-10 20:42:15 +0530

    Time: 20 ms (77.89%), Space: 51.3 MB (10.96%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -17,12 +17,33 @@ class Solution {
         int n=text1.length();
         int m=text2.length();
         int[][] dp=new int[n+1][m+1];
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<m;i++){
+            dp[0][i]=0;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
 
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j]=-1;
+
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                 dp[i][j]=1+dp[i-1][j-1];
+                }
+
+                else dp[i][j]= 0 + Math.max(dp[i-1][j],dp[i][j-1]);
+
             }
         }
-        return f(n,m,text1,text2,dp);
+
+        return dp[n][m];
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return f(n,m,text1,text2,dp);
     }
 }
