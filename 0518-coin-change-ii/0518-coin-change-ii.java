216253ca2a109e33f5514ed49fa3597540b50b4f
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-11-14 03:08:04 +0530

    Time: 12 ms (32.69%), Space: 49.7 MB (13.59%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    int function(int index,int amount,int[] coins,int[][] dp){
+
+        if(index==0){
+            if(amount%coins[index]==0){
+                return dp[index][amount]=1;
+            }
+            else return dp[index][amount]=0;
+        }
+        
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        
+        //not take
+        int notTake=0+function(index-1,amount,coins,dp);
+        
+        //TAKE
+        int take=0;
+
+        if(coins[index]<=amount){
+            take=function(index,amount-coins[index],coins,dp);
+        }
+
+        return dp[index][amount]=take+notTake;
+
+    }
+
+    public int change(int amount, int[] coins) {
+        
+        int n=coins.length;
+        int[][] dp=new int[n][amount+1];
+        
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+
+        int ans=function(n-1,amount,coins,dp);
+
+        // if(ans>=1e9){
+        //     return -1;
+        // }
+        // else 
+        return ans;
+
+        
+    }
+}
