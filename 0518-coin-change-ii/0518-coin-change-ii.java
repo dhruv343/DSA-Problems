7a9ba2f8a241ba05f04f6d741f88fe8673a9e0b2
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-11-14 03:16:18 +0530

    Time: 7 ms (72.04%), Space: 41.3 MB (75.5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,40 +1,64 @@
 class Solution {
-    int function(int index,int amount,int[] coins,int[][] dp){
+    int function(int index, int amount, int[] coins, int[][] dp) {
 
-        if(index==0){
-            if(amount%coins[index]==0){
-                return dp[index][amount]=1;
-            }
-            else return dp[index][amount]=0;
-        }
-        
-        if(dp[index][amount]!=-1) return dp[index][amount];
-        
-        //not take
-        int notTake=function(index-1,amount,coins,dp);
-        
-        //TAKE
-        int take=0;
-
-        if(coins[index]<=amount){
-            take=function(index,amount-coins[index],coins,dp);
+        if (index == 0) {
+            if (amount % coins[index] == 0) {
+                return dp[index][amount] = 1;
+            } else
+                return dp[index][amount] = 0;
         }
 
-        return dp[index][amount]=take+notTake;
+        if (dp[index][amount] != -1)
+            return dp[index][amount];
+
+        // not take
+        int notTake = function(index - 1, amount, coins, dp);
+
+        // TAKE
+        int take = 0;
+
+        if (coins[index] <= amount) {
+            take = function(index, amount - coins[index], coins, dp);
+        }
+
+        return dp[index][amount] = take + notTake;
 
     }
 
     public int change(int amount, int[] coins) {
-        
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-        
-        
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+
+        int n = coins.length;
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
+
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                prev[i] = 1;
+            } else
+                prev[i] = 0;
         }
 
-        return function(n-1,amount,coins,dp);
+        for (int index = 1; index < n; index++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int notTake = prev[amt];
+                // TAKE
+                int take = 0;
+
+                if (coins[index] <= amt) {
+                    take = curr[amt - coins[index]];
+                }
+
+                curr[amt] = take + notTake;
+            }
+            System.arraycopy(curr,0,prev,0,amount+1);
+        }
+
+        return prev[amount];
+        // for(int[] arr:dp){
+        // Arrays.fill(arr,-1);
+        // }
+
+        // return function(n-1,amount,coins,dp);
 
     }
 }
