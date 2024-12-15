87963ef02fdec609fc623a141bb28910805aa06c
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-12-15 11:13:07 +0530

    Time: 1326 ms (5.81%), Space: 54.3 MB (5.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        int length=nums.length;
-        List<List<Integer>> answer=new ArrayList<>();
+        int n = nums.length;
+        HashSet<List<Integer>> set = new HashSet<>();
 
-        Arrays.sort(nums);
-
-        for(int i=0;i<length-2;i++){
-            int j=i+1;
-            int k=length-1;
-
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    List<Integer> ele=new ArrayList<>();
-                    ele.add(nums[i]);
-                    ele.add(nums[j]);
-                    ele.add(nums[k]);
-                    answer.add(ele);
-
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    while(j<k && nums[j]==nums[j+1])k--;
-                    j++;
-                    k--;
-                }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    k--;
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> sett = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int third = -(nums[i] + nums[j]);
+                if (sett.contains(third)) {
+                    List<Integer> ans = new ArrayList<>();
+                    ans.add(third);
+                    ans.add(nums[i]);
+                    ans.add(nums[j]);
+                    Collections.sort(ans);
+                    set.add(ans);
+                } else {
+                    sett.add(nums[j]);
                 }
             }
         }
 
-        return answer;
-        
+        return new ArrayList(set);
     }
 }
