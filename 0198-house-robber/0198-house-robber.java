4960b8cd27f246461b9de25d515cbcb6962ccfd3
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-12-15 21:48:48 +0530

    Time: 0 ms (100%), Space: 41.4 MB (7.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,44 @@
 class Solution {
 
-    int func(int n,int[] dp,int[] arr){
+    int func(int n, int[] dp, int[] arr) {
 
-       if(n==0) return arr[0];
-       if(n<0) return 0;
+        if (n == 0)
+            return arr[0];
+        if (n < 0)
+            return 0;
 
-       if(dp[n]!=-1) return dp[n];
+        if (dp[n] != -1)
+            return dp[n];
 
-       int pick=arr[n]+func(n-2,dp,arr);
-       int notPick=func(n-1,dp,arr);
+        int pick = arr[n] + func(n - 2, dp, arr);
+        int notPick = func(n - 1, dp, arr);
 
-       return dp[n]=Math.max(pick,notPick);
+        return dp[n] = Math.max(pick, notPick);
     }
+
     public int rob(int[] nums) {
+
+        int n = nums.length;
+        int[] dp = new int[n];
         
-        int n=nums.length;
-        int[] dp=new int[n];
-        
-        Arrays.fill(dp,-1);
-        return func(n-1,dp,nums);
+
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            
+            if(i>1){
+                pick+=dp[i-2];
+            }
+
+            int notPick = dp[i-1];
+
+            dp[i] = Math.max(pick, notPick);
+        }
+
+        return dp[n-1];
+
+        // Arrays.fill(dp,-1);
+        // return func(n-1,dp,nums);
     }
 }
