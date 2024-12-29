e7a06d5f42eae8ae9c6cc4dd47982637e02a3037
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-12-29 15:03:27 +0530

    Time: 1 ms (92.83%), Space: 45.9 MB (36.64%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,60 +1,20 @@
 class Solution {
-
-    public int function(int index, int canBuy, int[] prices, int[][] dp) {
-
-        if (index == prices.length) {
-            return 0;
-        }
-
-        if (dp[index][canBuy] != -1)
-            return dp[index][canBuy];
-
-        int profit;
-
-        if (canBuy == 1) {
-            profit = Math.max(-prices[index] + function(index + 1, 0, prices, dp), function(index + 1, 1, prices, dp));
-        } else {
-            profit = Math.max(prices[index] + function(index + 1, 1, prices, dp), function(index + 1, 0, prices, dp));
-        }
-
-        return dp[index][canBuy] = profit;
-    }
-
     public int maxProfit(int[] prices) {
+        
+        int shareBought=prices[0];
+        int profit=0;
 
-        // int[][] dp = new int[prices.length + 1][2];
+        for(int price:prices){
+            int diff=price-shareBought;
 
-        int[] ahead=new int[2];
-        int[] curr=new int[2];
-
-        int n = prices.length;
-
-        ahead[0]=ahead[1] = 0;
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int canBuy = 0; canBuy <= 1; canBuy++) {
-                
-                int profit;
-
-                if (canBuy == 1) {
-                    profit = Math.max(-prices[index] + ahead[0],
-                            ahead[1]);
-                } else {
-                    profit = Math.max(prices[index] + ahead[1],
-                             ahead[0]);
-                }
-
-                curr[canBuy]=profit;
-
+            if(diff>0){
+                profit+=diff;
+                shareBought=price;
             }
-            System.arraycopy(curr,0,ahead,0,2);
+
+            shareBought=Math.min(shareBought,price);
         }
 
-        return ahead[1];
-
-        // for(int[] arr:dp){
-        // Arrays.fill(arr,-1);
-        // }
-        // return function(0,1,prices,dp);
+        return profit;
     }
 }
