0b9532d65e2587fd0645cc7e4327c6184932ae2a
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2024-12-31 05:37:07 +0530

    Time: 10 ms (77.39%), Space: 55.3 MB (26.15%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,17 +1,17 @@
 class Solution {
 
-    public int func(int[] nums, int k){
-
+    private int func(int[] nums, int k){
         int l=0;
         int r=0;
-        int count=0;
-        int sum=0;
         int n=nums.length;
-        if(k<0) return 0;
+        int count=0;
+        int totalK=0;
+
         while(r<n){
-            sum+=(nums[r]%2);
-            while(sum>k){
-                sum-=(nums[l]%2);
+            totalK+=(nums[r]%2);
+            
+            while(totalK>k){
+                totalK-=(nums[l]%2);
                 l++;
             }
 
@@ -20,7 +20,6 @@ class Solution {
         }
 
         return count;
-
     }
     public int numberOfSubarrays(int[] nums, int k) {
         return func(nums,k)-func(nums,k-1);
