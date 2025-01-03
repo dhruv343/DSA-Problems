e16b70311de2f5a63b3d90dcecf88e842c40edbb
Dhruv Gupta <118008255+dhruv343@users.noreply.github.com>
2025-01-04 01:51:28 +0530

    Time: 19 ms (88.22%), Space: 50.8 MB (58.84%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -21,14 +21,6 @@ class Solution {
 
         int[][] dp=new int[n+1][m+1];
 
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
-        
 
         for(int i=1;i<=n;i++) {
             for(int j=1;j<=m;j++) {
